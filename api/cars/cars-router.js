51c7cfe1033f439744164724315f92cf2742ce77
abemm7787 <mirambeaux7@gmail.com>
2021-08-10 14:54:03 -0500

    routes updates

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,64 @@
 // DO YOUR MAGIC
+const router = require("express").Router()
+const Cars = require("./cars-model")
+const {} =require("./cars-middleware")
+
+router.get("/", (req,res,next) =>{
+    // Cars.get()
+    // .then((mx) =>{
+    //     res.json(mx)
+    // })
+    // .catch(next)
+
+console.log("Vibrant Red")
+
+
+})
+router.get("/:id", (req,res,next) =>{
+    // Cars.get()
+    // .then((mx) =>{
+    //     res.json(mx)
+    // })
+    // .catch(next)
+
+
+
+
+console.log("Vibrant Red")
+
+})
+router.post("/", (req,res,next) =>{
+    // Cars.get()
+    // .then((mx) =>{
+    //     res.json(mx)
+    // })
+    // .catch(next)
+
+console.log("Vibrant Red")
+
+})
+router.put("/", (req,res,next) =>{
+    // Cars.get()
+    // .then((mx) =>{
+    //     res.json(mx)
+    // })
+    // .catch(next)
+
+console.log("Vibrant Red")
+
+})
+
+
+router.delete("/", (req,res,next) =>{
+    // Cars.get()
+    // .then((mx) =>{
+    //     res.json(mx)
+    // })
+    // .catch(next)
+
+console.log("Vibrant Red")
+
+})
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,6 +1,20 @@
 const express = require("express")
-
 const server = express()
+// const helmet = require("helmet")
+const cors = require("cors")
+
+
+
+const carRouter = require("./cars/cars-router")
+
+
+server.use("/api/cars" , carRouter)
+
+server.use(cors())
+// server.use(helmet())
+
+
+
 
 // DO YOUR MAGIC
 
